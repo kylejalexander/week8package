f99b210f10ce93c204d0bfa6e1127634c768e7cb
Kyle Alexander <kylejohnalexander@gmail.com>
2024-10-14 12:06:41 -0400

    Initial commit of week 8 assignment package

---
+++ R/triangle_side.R
@@ -0,0 +1,58 @@
+#' Calculate Triangle Side Length with Pythagorean Theorem
+#'
+#' This function calculates the length of the third side of a right triange,
+#'  given the lengths of the other two sides.
+#'
+#' @param side_a Numeric. The length of the first side of the triangle
+#' @param side_b Numeric. The length of the second side of the triangle
+#' @param side_c Numeric. The length of the hypotenuse (third) side of the triangle
+#' @return Numeric. The result of the pythagorean theorem calculating the missing side
+
+#' @examples
+#' Calculate the hypotenuse of a triangle
+#' triangle_side(side_a = 3, side_b = 4)  #Returns 5
+
+#' Calculate side b of a triangle
+#' triangle_side(side_a = 3, side_c = 5)  #Returns 4
+
+#' triangle_side
+#' @export
+triangle_side <- function(side_a = NULL, side_b = NULL, side_c = NULL) {
+
+  #' Checks if only two sides were inputted into the function
+  sides_input <- sum(!is.null(c(side_a, side_b, side_c)))
+  if (sides_input != 2){
+    stop("You must input exactly two sides")
+  }
+
+  #' Checking if slide lengths are positive
+  if (any(c(side_a, side_b, side_c) < 0)) {
+    stop("Side lengths must be positive.")
+  }
+
+
+  #' Calculates hypotenuse from sides a and b
+  if(is.null(side_c)){
+    return( sqrt( side_a^2 + side_b^2 ))
+  }
+
+  #' calculates side a if side b and hypotenuse (side c) are given.
+  #' ALso checks that side c is bigger than side b
+  if(is.null(side_a)){
+    if(side_c - side_b <= 0){
+      stop("Side c must be bigger than side b.")
+    }
+    return( sqrt( side_c^2 - side_b^2))
+  }
+
+  #' calculates side b if side a and hypotenuse (side c) are given
+  #' ALso checks that side c is bigger than side a
+  if(is.null(side_b)){
+    if(side_c - side_a <= 0){
+      stop("Side c must be bigger than side a.")
+    }
+    return( sqrt( side_c^2 - side_a^2))
+  }
+
+
+}
---
+++ R/trimmed_mean.R
@@ -0,0 +1,35 @@
+#' Calculates the Trimmed Mean
+#'
+#' This function calculates the trimmed mean of a numeric vector
+#' (e.g. mean that ignores the smallest and largest values)
+#'
+#' @param x A numeric vector. From this vector the trimmed mean is calculated.  The length of the first side of the triangle
+#' @param s Numeric. Number of smaller values to remove from the calculation
+#' @param l Numeric. Number of larger values to remove from the calculation
+#' @return Numeric. The trimmed mean
+
+#' @examples
+#' Calculate the trimmed mean
+#' trimmed_mean(x = c(1,7,3,2,5,0.5,9,10), s = 1, l = 2)  #Returns mean of c(1,7,3,2,5) = 3.6
+
+
+#' trimmed_mean
+#' @export
+trimmed_mean <- function(x = NULL, s = 0, l = 0) {
+
+  #' check that the length is longer than s + l
+  if (length(x) <= (s + l)) {
+    stop("Input vector must be larger than s + l.")
+  }
+
+  #' arranging from smallest to largest
+  arranged_vector <- sort(x)
+
+  #' trimming the vector to exclude the first s values and last l values
+  trimmed_vector <- arranged_vector[(s+1):(length(arranged_vector)-1)]
+
+  #' returning the mean
+  return(mean(trimmed_vector))
+
+
+}
